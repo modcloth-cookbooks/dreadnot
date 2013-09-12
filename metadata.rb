3984c38df8a6d606fed406bc0b96a60eee07961c
Dan Buch <d.buch@modcloth.com>
2013-09-12 14:35:46 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+name             'dreadnot'
+maintainer       'ModCloth, Inc.'
+maintainer_email 'github+dreadnot-cookbook@modcloth.com'
+license          'MIT'
+description      'Installs/Configures dreadnot'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
